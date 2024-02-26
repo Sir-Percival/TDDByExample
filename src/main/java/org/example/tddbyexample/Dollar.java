111cf355f9f1a4d4070849db301cf009f593edc3
Kacper Walichnowski <kacperw112@wp.pl>
2024-02-26 14:49:23 +0100

    Add Dollar class and multiplication test

---
+++ src/main/java/org/example/tddbyexample/Dollar.java
@@ -0,0 +1,16 @@
+package org.example.tddbyexample;
+
+public class Dollar
+{
+    int amount;
+
+    public Dollar(int amount)
+    {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier)
+    {
+        return new Dollar(amount * multiplier);
+    }
+}
